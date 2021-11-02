323a6286a4db574ac73e6383df26cd86a6131c7a
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-11-02 15:04:19 -0300

    Add router

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	controller "github.com/DantasB/Siga-Professor/Controller"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/professors", controller.GetProfessors).Methods("GET")
+	router.HandleFunc("/professors/{id}", controller.GetProfessor).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+
+}
