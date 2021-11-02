5ba5665d329620b58c790cb14f14045e60b96085
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-11-02 15:17:07 -0300

    Add utils package

---
+++ Utils/utils.go
@@ -0,0 +1,9 @@
+package utils
+
+func ToUtf8(iso8859_1_buf []byte) string {
+	buf := make([]rune, len(iso8859_1_buf))
+	for i, b := range iso8859_1_buf {
+		buf[i] = rune(b)
+	}
+	return string(buf)
+}
