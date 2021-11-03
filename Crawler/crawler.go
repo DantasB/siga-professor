7e2c0bebfdc2745553c411f247a90ac865c4b59b
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-11-03 01:09:25 -0300

    Update crawler.go

--- Crawler/crawler.go
+++ Crawler/crawler.go
@@ -44,67 +44,73 @@ func getSiraCoursesList() ([]string, error) {
 	return courses, nil
 }
 
-func AccessSiraCourses() {
+func AccessSiraCourses() ([][]string, error) {
+	result := [][]string{{}}
 	courses, err := getSiraCoursesList()
 	if err != nil {
 		log.Fatalln(err)
+		return [][]string{}, err
 	}
 
 	for _, course := range courses {
-		accessSiraCourse(course)
+		data, err := accessSiraCourse(course)
+		if err != nil {
+			log.Fatalln(err)
+			return [][]string{}, err
+		}
+		result = append(result, data...)
 	}
+
+	return result, nil
 }
 
-func accessSiraCourse(courseUrl string) ([]string, error) {
-	var disciplines []string
+func accessSiraCourse(courseUrl string) ([][]string, error) {
 	resp, err := http.Get(siraUrl + courseUrl)
 	if err != nil {
 		log.Fatalln(err)
-		return []string{}, err
+		return [][]string{}, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
-		return []string{}, err
+		return [][]string{}, err
 	}
 
 	htmlDocument, err := htmlquery.Parse(strings.NewReader(utils.ToUtf8(body)))
 	if err != nil {
 		log.Fatalln(err)
-		return []string{}, err
+		return [][]string{}, err
 	}
 
-	if !courseUpdated(htmlDocument) {
-		return []string{}, nil
+	err = courseUpdated(htmlDocument)
+	if err != nil {
+		log.Fatalln(err)
+		return [][]string{}, nil
 	}
 
-	if !parseCourseInformation(htmlDocument, &disciplines) {
-		return []string{}, nil
-	}
-
-	return disciplines, nil
+	return parseDisciplines(htmlDocument), nil
 }
 
-func parseCourseInformation(htmlDocument *html.Node, disciplines *[]string) bool {
-	parseDisciplines(htmlDocument, disciplines)
-	return true
-}
-
-func parseDisciplines(htmlDocument *html.Node, disciplines *[]string) {
+func parseDisciplines(htmlDocument *html.Node) [][]string {
+	disciplines := [][]string{}
 	disciplineNodes := htmlquery.Find(htmlDocument, "//td//td//table[@class='lineBorder']//table//tr[@class != 'tableTitle']")
 	for _, node := range disciplineNodes {
 		line := htmlquery.InnerText(node)
+
 		line = utils.ReplaceMultipleSpacesByPipe(utils.RemoveSeparators(line))
+		line = line[1 : len(line)-1] //Remove the first and last pipe character
 		if strings.Contains(line, "Lista de Disciplinas") || strings.Contains(line, "Nome Turma") {
 			continue
 		}
 
-		fmt.Printf("%s\n", line)
+		disciplines = append(disciplines, strings.Split(line, "|"))
 	}
+
+	return disciplines
 }
 
-func courseUpdated(htmlDocument *html.Node) bool {
+func courseUpdated(htmlDocument *html.Node) error {
 	courseNode := htmlquery.FindOne(htmlDocument, "//font[@size=3]/text()")
 	courseName := strings.TrimSpace(htmlquery.InnerText(courseNode))
 	courseName = strings.TrimSpace(strings.Split(courseName, "-")[0])
@@ -113,15 +119,15 @@ func courseUpdated(htmlDocument *html.Node) bool {
 	datetime := strings.TrimSpace(htmlquery.InnerText(htmlNode))
 	parsedTime, err := utils.ParseDate(datetime)
 	if err != nil {
-		return false
+		return err
 	}
 
 	currentYear, currentMonth, _ := time.Now().Date()
 	lastUpdateYear, lastUpdateMonth, _ := parsedTime.Date()
 	if currentYear > lastUpdateYear || currentMonth > lastUpdateMonth+1 {
 		fmt.Printf("[%s] This is an old course and should be ignored. %s\n", courseName, datetime)
-		return false
+		return nil
 	}
 
-	return true
+	return nil
 }
